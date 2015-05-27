373043d353905d6280577282a7a5331f5a279eb6
brett <mail@brettklamer.com>
2015-05-27 14:09:43 -0400

    Build script no longer depends on user input

--- build-site.r
+++ build-site.r
@@ -1,41 +1,14 @@
 library(rmarkdown)
 
-# Type in the .rmd filenames in the order you wish them to be displayed.
-rmdFileNames <- c(
-  "index.rmd", 
-  "example1.rmd",
-  "example2.rmd",
-  "example3.rmd"
-)
-
-# Fixes directory structure and remove any old build directory.
+# Read in toc, create file names, and remove old build folder.
+rmdFileNames <- read.csv("rmd/toc.csv", header = FALSE)
+rmdFileNames <- as.character(rmdFileNames$V1)
 htmlFileNames <- gsub(".rmd", ".html", rmdFileNames, fixed = TRUE)
 rmdFileNames <- paste("rmd/", rmdFileNames, sep="")
 unlink("build", recursive = TRUE)
 
-# Render the .rmd files to html. Type in your desired pandoc metadata.
-lapply(
-  rmdFileNames, 
-  rmarkdown::render, 
-  output_dir = "../build",
-  html_document(
-    smart = TRUE,
-    self_contained = FALSE,
-    lib_dir = "../www/assets",
-    template = "../www/templates/default.html",
-    theme = NULL,
-    highlight = NULL,
-    mathjax = NULL,
-    pandoc_args = c(
-      "--metadata", "booktitle=Book Title",
-      "--metadata", "link1=http://example.org",
-      "--metadata", "linktext1=example.org",
-      "--metadata", "link2=http://example.org",
-      "--metadata", "linktext2=example.org",
-      "--metadata", "lang=en"
-    )
-  )
-)
+# Renders the rmd files to html.
+lapply(rmdFileNames, rmarkdown::render, output_dir = "../build")
 
 # Insert the table of contents and page navigation.
 source("scripts/toc.r")
