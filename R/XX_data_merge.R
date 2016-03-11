ef572a1c19af90df4c02370cb1f14422724802ed
j_n <tupiszakaczki@gmail.com>
2016-03-11 22:59:53 +0100

    remake needs to be fought
    Former-commit-id: 0b244ddfd663c5f2d046f8bdbdc90364b0026782
    Former-commit-id: f1e8b49cdf133b319e03ca1c9cf6c8dab5428a4e

--- R/XX_data_merge.R
+++ R/XX_data_merge.R
@@ -1,3 +1,21 @@
-data_merger <- function(){
+data_merger <- function(spk, clc, srtm, temp, ndvi, savi){
+        ext <- extent(spk)
+        rtemplate <- raster(ext)
+        res(rtemplate) <- 30
+        clc_res <- resample(raster(clc), rtemplate, method="ngb")
+        srtm_res <- resample(raster(srtm), rtemplate, method="bilinear")
+        temp_res <- resample(raster(temp), rtemplate, method="bilinear")
+        ndvi_res <- resample(raster(ndvi), rtemplate, method="bilinear")
+        savi_res <- resample(raster(savi), rtemplate, method="bilinear")
+        stack(clc_res, srtm_res, temp_res, ndvi_res, savi_res)
         
 }
+
+# raster_to_sp <- function(raster_stack){
+#         
+# }
+
+# 
+# 
+# rs <- readRDS('data/raster_stack.rds')
+# names(rs)
