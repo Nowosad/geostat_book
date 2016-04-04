156f64d69dbd1d68efc32812fbb775928f741de4
j_n <tupiszakaczki@gmail.com>
2016-04-04 21:28:47 +0200

    cover, mobi, pdfa4 added
    Former-commit-id: 35444cb289f1feffd834265fbbb9bdc74ad20897
    Former-commit-id: 8280273dd898f0b77bb7682648679e79c505d07b

---
+++ Rfigs/cover.R
@@ -0,0 +1,31 @@
+# devtools::install_github('nowosad/geostatbook')
+# devtools::install_github('nowosad/wesanderson')
+# devtools::install_github('oscarperpinan/rasterVis')
+library('gridExtra')
+library('ggplot2')
+library('sp')
+library('wesanderson')
+library('geostatbook')
+library('raster')
+library('rasterVis')
+library('viridis')
+
+data('siatka')
+data('granica')
+data('punkty')
+siatka <- raster(siatka)
+
+myTheme <- rasterTheme(region=rev(viridis(256)), strip.background = list(col = 'transparent'))
+# myTheme <- rasterTheme(region=rev(viridis(256)), axis.line = list(col = "transparent"))
+plotek <- levelplot(siatka, contour=FALSE, par.settings=viridisTheme, zscaleLog = TRUE,
+          margin=FALSE, scales=list(draw=FALSE), colorkey=FALSE) +
+        layer(sp.points(punkty, pch=20, col='black')) +
+        layer(sp.lines(granica, lwd=4, col='#999999')) 
+plotek <- arrangeGrob(plotek)
+        
+ggsave(plotek, file='Rfigs/cover.png')
+# library('ggplot2')
+# gplot(siatka) + geom_tile(aes(fill = value)) +
+#     facet_wrap(~ variable) +
+#     scale_fill_gradient(low = 'white', high = 'blue') +
+#     coord_equal()
--- temp2.R
+++ temp2.R
@@ -1,1 +1,2 @@
-bookdown::render_book('index.Rmd', output_format='bookdown::pdf_book')
+bookdown::render_book('index.Rmd', output_dir='pdf', 'bookdown::pdf_book')
+bookdown::render_book('index.Rmd', output_dir='pdf', 'bookdown::epub_book')
