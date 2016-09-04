bb8a04d44e10952eb2738948299737daffb1a160
nowosad <tupiszakaczki@gmail.com>
2016-09-03 20:48:38 -0400

    update images in pdf
    Former-commit-id: 1d3b968ad5441fef90bcc5cd7c667177a6c4b06f
    Former-commit-id: 1847d278040dbaacef17822fa31abe005e63740c

--- temp.R
+++ temp.R
@@ -8,18 +8,23 @@ beepr::beep()
 # devtools::install_github('rstudio/bookdown')
 # bookdown::publish_book(render='none')
 install.packages('beepr')
-beepr::beep()
+beepr::beep(0)
 
 #pdf
 Sys.setenv(RSTUDIO_PANDOC="/usr/lib/rstudio/bin/pandoc")
 
 bookdown::render_book("index.Rmd", "bookdown::pdf_book", output_dir='pdf_ania')
-beepr::beep()
+beepr::beep(0)
+
+bookdown::render_book(input="index.Rmd")
+bookdown::publish_book(name='Geostatystyka', account='nowosad')
+beepr::beep(0)
 
 #ebook
 bookdown::render_book("index.Rmd", "bookdown::epub_book")
 beepr::beep()
 
 #clean
+bookdown::clean_book(FALSE)
 bookdown::clean_book(TRUE)
 
