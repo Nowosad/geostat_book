41b642904070f73b565a8974be5de5fcb043d1f6
jn <tupiszakaczki@gmail.com>
2017-11-25 18:53:05 -0500

    update build
    Former-commit-id: 85ed7e81a5ebd94eeb03867c482adf2ba4f9d1fc
    Former-commit-id: 30cb6cd89784da4e111c32fed6931170aa5c6b8f

--- _build.sh
+++ _build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env Rscript
-bookdown::render_book("index.rmd")
+bookdown::render_book("index.Rmd")
+bookdown::publish_book(name = "Geostatystyka", account = "nowosad", render = c("none"), server = "bookdown.org")
