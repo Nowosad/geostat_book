0e2ac61aad34a4bf285db436d504db75c458a037
jn <tupiszakaczki@gmail.com>
2017-11-25 17:57:40 -0500

    cleaned before script
    Former-commit-id: dec90daa9dec3a256d625ba3e6a8d7232c3921e3
    Former-commit-id: 3c7619dd0da7e5fa5bb43e02139c6e8c07f21375

--- code/before_script.R
+++ code/before_script.R
@@ -1,7 +1,4 @@
-library("knitr")
 library("methods")
-library("Cairo")
-library("grDevices")
 knitr::opts_chunk$set(warning = FALSE, message = FALSE, out.width = '\\textwidth')
-options(device = "cairo_pdf", scipen = 99 * 99)
+options(scipen = 99 * 99)
 
