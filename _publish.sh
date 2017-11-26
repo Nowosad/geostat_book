9cbda292a2f528dc3521b43964f49dd340dd6ce7
jn <tupiszakaczki@gmail.com>
2017-11-25 20:48:00 -0500

    published update
    Former-commit-id: 1eb3e0178b1d21a2ef39e7834e6e1568af9f28c9
    Former-commit-id: 9dbaef33f49574e9ad69901df4c5c08537f3b72d

--- _publish.sh
+++ _publish.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env Rscript
+if (!require(rsconnect)) install.packages('rsconnect')
 bookdown::publish_book(name = "Geostatystyka", account = "nowosad", render = c("none"), server = "bookdown.org")
