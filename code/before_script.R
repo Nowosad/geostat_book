b7a2a7b1095921c4b272f7a82452c3de9e27f148
jn <tupiszakaczki@gmail.com>
2018-10-09 09:32:41 +0200

    fig size updated

--- code/before_script.R
+++ code/before_script.R
@@ -3,6 +3,6 @@ knitr::opts_chunk$set(warning = FALSE,
                       message = FALSE,
                       out.width = "\\textwidth",
                       fig.align = "center",
-                      fig.width = 8,
+                      fig.width = 9,
                       fig.show = "hold")
 options(scipen = 99 * 99)
