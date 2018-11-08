c0476818536dbfd276a7903039554bf16dab73e5
jn <tupiszakaczki@gmail.com>
2018-11-08 14:17:33 +0100

    try CairoPNG

--- _publish.sh
+++ _publish.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env Rscript
-if (!require(rsconnect)) install.packages("rsconnect")
-if (!require(svglite)) install.packages("svglite")
+if (!require(rsconnect)) install.packages("Cairo")
 bookdown::publish_book(name = "Geostatystyka", account = "nowosad", render = c("none"), server = "bookdown.org")
