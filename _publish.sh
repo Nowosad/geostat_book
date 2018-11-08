b8c815686de838703b3682d90dd2ff532554992f
jn <tupiszakaczki@gmail.com>
2018-11-08 14:05:48 +0100

    try different device

--- _publish.sh
+++ _publish.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env Rscript
 if (!require(rsconnect)) install.packages("rsconnect")
+if (!require(svglite)) install.packages("svglite")
 bookdown::publish_book(name = "Geostatystyka", account = "nowosad", render = c("none"), server = "bookdown.org")
