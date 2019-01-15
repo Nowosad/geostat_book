30d6e00092b83e00dc0852ce2bbb21c71e385e8b
jn <tupiszakaczki@gmail.com>
2019-01-15 12:44:36 +0100

    removes pdf build

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
 if (!require(Cairo)) install.packages("Cairo")
 if (!require(bookdown)) install.packages("bookdown")
-bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book")
+# bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book")
 bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook", clean = FALSE)
