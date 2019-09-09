ecc0a201a6df88baa38a4c4adb49960acb797b43
Nowosad <tupiszakaczki@gmail.com>
2019-09-09 15:50:28 +0200

    improves befora script

--- code/before_script.R
+++ code/before_script.R
@@ -1,4 +1,6 @@
 library(methods)
+library(ggplot2)
+library(rcartocolor)
 knitr::opts_chunk$set(warning = FALSE,
                       message = FALSE,
                       out.width = "\\textwidth",
@@ -6,3 +8,11 @@ knitr::opts_chunk$set(warning = FALSE,
                       fig.width = 9,
                       fig.show = "hold")
 options(scipen = 99 * 99)
+p1_base = ggplot() +
+        coord_equal() +
+        rcartocolor::scale_fill_carto_c(palette = "ag_Sunset",
+                                        na.value = "#ffffff") +
+        theme_void() +
+        scale_x_discrete(expand = c(0, 0)) +
+        scale_y_discrete(expand = c(0, 0)) +
+        labs(fill = "")
