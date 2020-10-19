d4a4a2c044f539930b66aaef8b5e313551309ac0
Nowosad <tupiszakaczki@gmail.com>
2020-10-19 20:05:54 +0200

    improves diag

--- code/diag.R
+++ code/diag.R
@@ -3,21 +3,26 @@ library(DiagrammeRsvg)
 library(magrittr)
 library(rsvg)
 
-p = DiagrammeR::mermaid("
+pg = DiagrammeR::mermaid('
                 graph TB;
                 Pozyskanie[<center>Pozyskanie <br>i wstępna weryfikacja danych</center>];
-                Pozyskanie-->Eksploracja[<center>Nieprzestrzenna i przestrzenna <br>Eksploracja danych</center>];
+                Pozyskanie-->Eksploracja[<center>Nieprzestrzenna i przestrzenna <br>eksploracja danych</center>];
                 Eksploracja-->Analiza[<center>Analiza <br>i interpretacja <br>struktury przestrzennej</center>];
-                Analiza -->Modelowanie[<center>Modelowanie matematyczne <br>struktury przestrzennej</center>];
+                Analiza -->Modelowanie["<center>Modelowanie matematyczne <br>struktury przestrzennej (autokorelacji przestrzennej) </center>"];
+                
                 Modelowanie -->Estymacja;
                 Modelowanie -->Optymalizacja;
+                Estymacja -->Symulacja;
+                
                 Modelowanie -->Symulacja;
-                ")
+                Estymacja -->Optymalizacja;
+                Symulacja -->Optymalizacja;
+                Optymalizacja -->Pozyskanie;
+                
+                ')
 
-# plotly::export(p,
+pg
+# plotly::export(pg,
 #                file = "figs/diag.png",
 #                vwidth = 466,
 #                vheight = 529)
-
-
-
