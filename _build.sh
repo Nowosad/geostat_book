21f4099aa3cf5f7d01f95122272d295ec521253e
Jakub Nowosad <tupiszakaczki@gmail.com>
2021-10-02 14:58:05 +0200

    adds bs4_book

--- _build.sh
+++ _build.sh
@@ -2,4 +2,5 @@
 if (!require(Cairo)) install.packages("Cairo")
 if (!require(bookdown)) install.packages("bookdown")
 bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book")
-bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook", clean = FALSE)
+# bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook", clean = FALSE)
+bookdown::render_book("index.Rmd", output_format = "bookdown::bs4_book", clean = FALSE)
